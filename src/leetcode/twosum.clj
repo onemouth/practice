44cf4fb678f10052c56606f8d6a1ede39606b629
LT <lt@ltt.pw>
2022-10-01 00:36:32 +0800

    two sum

--- src/leetcode/twosum.clj
+++ src/leetcode/twosum.clj
@@ -1,18 +1,20 @@
 (ns leetcode.twosum)
 
-
 (defn twosum [nums target]
   (let [nums-indexed (map-indexed vector nums)
-        target-map (reduce #(assoc %1 (- target (%2 1)) (%2 0)) (hash-map) nums-indexed)]
-    ; %2: [idx, value]
-    (reduce #(if (contains? target-map (%2 1))
-               (reduced [(%2 0) (target-map (%2 1))])
-               %1)
+        target-map (atom (hash-map))]
+    (reduce (fn [_ [idx val]] (if (contains? @target-map val)
+                                (reduced [(@target-map val) idx])
+                                (do (swap! target-map assoc (- target val) idx)
+                                    [])))
             [] nums-indexed)))
 
 (= (twosum [2 7 11 15] 9) [0 1])
 
+; [1 2]
 (twosum [3 2 4] 6)
 
+; [0 1]
+(twosum [3 3] 6)
 
-; target-map (keep-indexed #(assoc target-map (- target %2) %1) nums)
+(twosum [3 3] 7)
