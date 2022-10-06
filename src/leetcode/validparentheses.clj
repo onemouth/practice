866f9f5c49313033d80d6ca9f9913df8d6d291ea
LT <lt@ltt.pw>
2022-10-06 21:40:27 +0800

    add validparentheses

--- src/leetcode/validparentheses.clj
+++ src/leetcode/validparentheses.clj
@@ -7,16 +7,12 @@
 
 
 (defn is-valid [s]
-  (let [stack (atom '())]
-    (reduce (fn [_ ch]
-              (cond
-                (or (= ch \{)
-                    (= ch \()
-                    (= ch \[)) (do (swap! stack conj (par-map ch))
-                                   false)
-                (not= (peek @stack) ch) (reduced false)
-                :else (do (swap! stack pop)
-                          (if (= (count @stack) 0)
-                            true
-                            false))))
-            true s)))
+  (let [stack (reduce (fn [stack ch]
+                        (cond
+                          (contains? par-map ch) (conj stack (par-map ch))
+                          (not= (peek stack) ch) (reduced stack)
+                          :else (pop stack)))
+                      '() s)]
+    (if (zero? (count stack))
+      true
+      false)))
